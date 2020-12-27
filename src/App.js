06771614cd196ce339fea93243c76ddd41ecfbe6
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-12-27 11:22:13 -0500

    🌙  Dark mode

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import { Router, Route } from 'react-router-dom';
 
 import history from './utils/history'; 
 import { ThemeProvider } from 'styled-components';
+import { useDarkMode } from './store/Mode/useDarkMode';
+import { darkMode, lightMode } from './store/Mode/theme';
+import { GlobalStyles } from './store/Mode/global';
 
 function App() {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+
+  const themeMode = theme === 'dark' ? darkMode : lightMode;
+
+  if (!componentMounted) {
+    return <div />;
+  }
+
   return (
     <Router history={history}>
-      <ThemeProvider>
+      <ThemeProvider theme={themeMode}>
         <div className="App">
+          <GlobalStyles/>
           Hello World
         </div>
       </ThemeProvider>
